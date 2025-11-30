4bbafadb81fc6d51462d362e97667fa97a64abfb
Aleksandr <pp2010@rambler.ru>
2025-11-30 19:13:28 +0300

    Initial commit

---
+++ src/main/kotlin/Lesson_1/Lesson1_task3.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson_1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println(year)
+    println("0$hour")
+    println("0$minute")
+    hour = 10
+    minute = 55
+    //println("$hour:$minute") как вариант)
+    print(hour)
+    print(":")
+    print(minute)
+}
