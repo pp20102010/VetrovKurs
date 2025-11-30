f20cd06e2e8e6ac4c652bbe971158452f7dc2c46
Aleksandr <pp2010@rambler.ru>
2025-11-30 18:52:03 +0300

    Initial commit

---
+++ src/main/kotlin/Lesson_1/Lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson_1
+
+fun main() {
+    var numberOfOrders: Int = 75
+    var textOfGratitude: String = "«Спасибо, что не прошли мимо! Ваш выбор – наша суперсила»."
+
+    println(numberOfOrders)
+    println(textOfGratitude)
+
+    var numberOfWorkers = 2000
+    //println(numberOfWorkers)
+    numberOfWorkers = 1999
+    println(numberOfWorkers)
+}
