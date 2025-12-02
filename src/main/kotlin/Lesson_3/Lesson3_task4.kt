e51f9d45a1627a824f8953e8d5d06c2e2db1c974
Aleksandr <pp2010@rambler.ru>
2025-12-02 17:30:04 +0300

    KS-3-4 Solution version 1

---
+++ src/main/kotlin/Lesson_3/Lesson3_task4.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson_3
+
+fun main() {
+    var n = 1
+    var s = "E2"
+    var f = "E4"
+    println("$s-$f;$n")
+    n = 3
+    s = "D2"
+    f = "D3"
+    println("$s-$f;$n")
+}
