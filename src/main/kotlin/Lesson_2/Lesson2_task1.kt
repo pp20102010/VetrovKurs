7f6d7c39e613aba4bd12a67ef1a759fb0e77193c
Aleksandr <pp2010@rambler.ru>
2025-12-02 17:12:44 +0300

    KS-2-1 Solution version 1

---
+++ src/main/kotlin/Lesson_2/Lesson2_task1.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson_2
+
+fun main() {
+
+    var s1 = 3
+    var s2 = 4
+    var s3 = 3
+    var s4 = 5
+
+    var ss = (s1 + s2 + s3 + s4) / 4.0
+    println(String.format("%.2f", ss))
+
+}
