31e7c1ade309624d08f9c0ce1cf72d59d7dcb0bd
Aleksandr <pp2010@rambler.ru>
2025-12-03 18:57:13 +0300

    KS-1-5 доработал по замечаниям

--- src/main/kotlin/Lesson_1/Lesson1_task5.kt
+++ src/main/kotlin/Lesson_1/Lesson1_task5.kt
@@ -1,10 +1,16 @@
 package org.example.Lesson_1
 
-fun main() {
-    val seconds: Int = 6480
-    val hour = String.format("%02d", seconds/3600)
-    val minute = String.format("%02d", seconds%3600/60)
-    val seconds2 = String.format("%02d", (seconds%3600)%60)
+const val SECONDS_IN_AN_MINUT = 60
+const val SECONDS_IN_AN_HOUR = 60 * 60
 
-    println("$hour:$minute:$seconds2")
+fun main() {
+    val totalSeconds: Int = 6480
+    val hours = totalSeconds / SECONDS_IN_AN_HOUR
+    val minutes = totalSeconds % SECONDS_IN_AN_HOUR / SECONDS_IN_AN_MINUT
+    val seconds = (totalSeconds % SECONDS_IN_AN_HOUR) % SECONDS_IN_AN_MINUT
+
+    //val seconds2 = String.format("%02d", (seconds % SECONDS_IN_AN_HOUR) % SECONDS_IN_AN_MINUT)
+    //println("$hour:$minute:$seconds2")
+
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
