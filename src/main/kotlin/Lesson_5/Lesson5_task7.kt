1af9d3a5efd881f8643c27f2923fd6a1017551ab
Aleksandr <pp2010@rambler.ru>
2025-12-08 14:22:26 +0300

    KS-5-7 Расчет топлива. Исправил форматирование

--- src/main/kotlin/Lesson_5/Lesson5_task7.kt
+++ src/main/kotlin/Lesson_5/Lesson5_task7.kt
@@ -7,6 +7,6 @@ fun main() {
     val consumption = readln().toFloat()
     print("Текущую цену за литр топлива(в рублях): ")
     val price = readln().toFloat()
-    val fuel = distance/100*consumption
-    println("Для поездки необходимо %.2fл. топлива (на сумму %.2fруб.)".format( fuel, fuel*price ))
+    val fuel = distance / 100 * consumption
+    println("Для поездки необходимо %.2fл. топлива (на сумму %.2fруб.)".format(fuel, fuel * price))
 }
